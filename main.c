e63ce8d75ba63d284799612f720d978092b0cb7f
student-hash <98327528+student-hash@users.noreply.github.com>
2022-02-21 15:56:15 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+Project:county library
+Auther :Kelvin
+Date : feb/2022
+Compiler: GNU GCC
+Language : C99
+Licence:MIT
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{printf("\tCounty Library\n");
+printf("Welcome Kelvin.\n");
+printf("What would you like to do?\n");
+printf("1. Add new user\n");
+printf("2. Add Book.\n");
+printf("Selected action: ");
+    return 0;
+}
